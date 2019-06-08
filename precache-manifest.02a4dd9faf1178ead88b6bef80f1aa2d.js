6a0f66e3b8fd21d9e26e47366ed6301f3fe12c2b
annelister <hithere@hithere.local>
2019-06-07 18:32:03 -0700

    Updates

--- precache-manifest.02a4dd9faf1178ead88b6bef80f1aa2d.js
+++ precache-manifest.02a4dd9faf1178ead88b6bef80f1aa2d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1708ff00337a15ff9c167952080d2e56",
+    "revision": "ed3e5e2907e1feba66bdfa30eaa0fb7a",
     "url": "/thelcipher/index.html"
   },
   {
-    "revision": "15b110519c21ad6a9839",
+    "revision": "1e64419ac6782a2738a0",
     "url": "/thelcipher/static/css/main.b07d1e75.chunk.css"
   },
   {
-    "revision": "faad09c39b0b9fdc6d4d",
-    "url": "/thelcipher/static/js/2.3da42d3d.chunk.js"
+    "revision": "42aa3b3576a7cb395377",
+    "url": "/thelcipher/static/js/2.543c33af.chunk.js"
   },
   {
-    "revision": "15b110519c21ad6a9839",
-    "url": "/thelcipher/static/js/main.da119b91.chunk.js"
+    "revision": "1e64419ac6782a2738a0",
+    "url": "/thelcipher/static/js/main.797c1e02.chunk.js"
   },
   {
     "revision": "2d3de663e04ff67c85a7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/thelcipher/precache-manifest.a92dbb43d74a6ba161b1b10b442e9dac.js"
+  "/thelcipher/precache-manifest.02a4dd9faf1178ead88b6bef80f1aa2d.js"
 );
 
 self.addEventListener('message', (event) => {
